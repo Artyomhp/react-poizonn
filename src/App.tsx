bd1b9324b186c062f80f695cf25da4a76956b2f6
Artyom Melikyan <rtmmlkn@inbox.ru>
2025-11-14 22:07:16 +0300

    create header component

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+
+import "./index.css";
+
+function App() {
+  return (
+    <div className="h-full">
+      <Header />
+      <div>hello</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+const Header = () => {
+  const menuItem = [
+    "Одежда",
+    "Обувь",
+    "Аксессуары",
+    "Калькулятор стоимости",
+    "Помощь",
+    "Подобрать размер",
+  ];
+  return (
+    <div className="h-25 my-3.75 ">
+      <div className="container mx-auto bg-white/50 border-b border-border">
+        <div className="flex items-center py-3.75">
+          <div className="flex items-center mr-18.75">
+            <img
+              width={50}
+              height={35}
+              src="./src/assets/images/logo.svg"
+              alt="logo"
+            />
+          </div>
+          <ul className="flex gap-15 mr-auto">
+            {menuItem.map((item, index) => (
+              <li key={index} className="text-lg font-normal text-gray-text">
+                {item}
+              </li>
+            ))}
+          </ul>
+          <div className="flex gap-10 ml-auto">
+            <div className="flex items-center">
+              <img src="./src/assets/images/icons/cart.svg" alt="cart" />
+            </div>
+            <div className="px-5.75 py-4.25 flex items-center gap-2.5 rounded-[15px] bg-btn-tg">
+              <div>
+                <img
+                  className="w-7.5 h-5"
+                  src="./src/assets/images/logo-white.svg"
+                  alt="logo"
+                />
+              </div>
+              <div>
+                <div className="flex flex-col ">
+                  <span className="text-base text-white font-semibold">
+                    Наш телеграм канал
+                  </span>
+                  <span className="text-[13px] text-white">
+                    Telegram web app
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
